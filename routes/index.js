acc63d1b53cb95a89d056256cd09124909e8978c
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2021-07-14 01:02:50 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,45 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  console.log(req);
+  let message = {
+    "version": "string",
+    "sessionAttributes": {
+      "key": "value"
+    },
+    "response": {
+      "outputSpeech": {
+        "type": "PlainText",
+        "text": "Plain text string to speak",
+        "playBehavior": "REPLACE_ENQUEUED"
+      },
+      "card": {
+        "type": "Standard",
+        "title": "Title of the card",
+        "text": "Text content for a standard card",
+        "image": {
+          "smallImageUrl": "https://url-to-small-card-image...",
+          "largeImageUrl": "https://url-to-large-card-image..."
+        }
+      },
+      "reprompt": {
+        "outputSpeech": {
+          "type": "PlainText",
+          "text": "Plain text string to speak",
+          "playBehavior": "REPLACE_ENQUEUED"
+        }
+      },
+      "directives": [
+        {
+          "type": "InterfaceName.Directive"
+        }
+      ],
+      "shouldEndSession": true
+    }
+  }
+
+  res.json(message);
 });
 
 module.exports = router;
