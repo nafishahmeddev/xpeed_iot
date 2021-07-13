f3ab3c911d464cbc4cc47e01a0255d78e3f9d98b
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2021-07-13 12:35:20 +0530

    first commit

--- routes/user.js
+++ routes/user.js
@@ -163,5 +163,15 @@ router.get("/me", auth, async (req, res) => {
     }
 });
 
+router.get("/devices", auth, async (req, res) => {
+    try {
+        // request.user is getting fetched from Middleware after token authentication
+        const user = await User.findById(req.user.id);
+        res.json(user);
+    } catch (e) {
+        res.send({ message: "Error in Fetching user" });
+    }
+});
+
 
 module.exports = router;
