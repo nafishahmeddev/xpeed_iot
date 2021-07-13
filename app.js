4cf46c939d89de914cc9b6a5f901be97aa69f666
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2021-07-13 23:34:56 +0530

    first commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var lessMiddleware = require('less-middleware');
 var logger = require('morgan');
 var cors = require('cors')
 const InitiateMongoServer = require("./config/db");
+
 /*****
  *
  * @type {Router}
@@ -50,5 +51,115 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
+/*****
+ * Ask SDK
+ */
+const Alexa = require('ask-sdk-core');
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'Welcome to your SDK weather skill. Ask me the weather!';
+
+    return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .withSimpleCard('Welcome to your SDK weather skill. Ask me the weather!', speechText)
+        .getResponse();
+  }
+};
+const AskWeatherIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+        && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AskWeatherIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'The weather today is sunny.';
+
+    return handlerInput.responseBuilder
+        .speak(speechText)
+        .withSimpleCard('The weather today is sunny.', speechText)
+        .getResponse();
+  }
+};
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+        && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can ask me the weather!';
+
+    return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .withSimpleCard('You can ask me the weather!', speechText)
+        .getResponse();
+  }
+};
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+        && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+            || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+        .speak(speechText)
+        .withSimpleCard('Goodbye!', speechText)
+        .withShouldEndSession(true)
+        .getResponse();
+  }
+};
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    // Any clean-up logic goes here.
+    return handlerInput.responseBuilder.getResponse();
+  }
+};
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+        .speak('Sorry, I don\'t understand your command. Please say it again.')
+        .reprompt('Sorry, I don\'t understand your command. Please say it again.')
+        .getResponse();
+  }
+};
+let skill;
+
+exports.handler = async function (event, context) {
+  console.log(`REQUEST++++${JSON.stringify(event)}`);
+  if (!skill) {
+    skill = Alexa.SkillBuilders.custom()
+        .addRequestHandlers(
+            LaunchRequestHandler,
+            AskWeatherIntentHandler,
+            HelpIntentHandler,
+            CancelAndStopIntentHandler,
+            SessionEndedRequestHandler,
+        )
+        .addErrorHandlers(ErrorHandler)
+        .create();
+  }
+
+  const response = await skill.invoke(event, context);
+  console.log(`RESPONSE++++${JSON.stringify(response)}`);
+
+  return response;
+};
 
 module.exports = app;
