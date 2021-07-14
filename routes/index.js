8931a836947b11145e61edd886592cd0ca3c10fb
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2021-07-14 10:09:43 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:path*', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  console.log(req);
+  console.log(req.query);
   let message = {
     "version": "string",
     "sessionAttributes": {
