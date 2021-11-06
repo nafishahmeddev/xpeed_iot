2b0e1fceca9123f57619f0ca0459ade9684955a3
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2021-11-06 11:06:53 +0530

    mix

--- app.js
+++ app.js
@@ -55,6 +55,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  res.json({
+    success:  false,
+    message: err.message,
+  })
 });
 
 
